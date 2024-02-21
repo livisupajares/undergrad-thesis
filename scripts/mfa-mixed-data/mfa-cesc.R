d2b8f775f4945117d2d654f0dac5105034fc292a
Livisu Pajares <livisupajares@gmail.com>
2024-02-21 12:21:11 -0500

    Load libraries on script

--- scripts/mfa-mixed-data/mfa-cesc.R
+++ scripts/mfa-mixed-data/mfa-cesc.R
@@ -3,3 +3,7 @@ if (!require(c("FactoMineR", "factoextra","ggplot2"))) {
   install.packages(c("FactoMineR", "factoextra","ggplot2"))
 }
 
+# Load libraries
+library("FactoMineR")
+library("factoextra")
+
